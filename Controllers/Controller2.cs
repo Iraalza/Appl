35820d480be65476415b2990688925b80c0663d0
Iraalza <43280192+Iraalza@users.noreply.github.com>
2022-05-18 00:51:35 +0300

    Update Controller2.cs

--- Controllers/Controller2.cs
+++ Controllers/Controller2.cs
@@ -1,26 +1,23 @@
-﻿using Appl.Models;
+using Appl.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Appl.Controllers
 {
+    [ApiController]
+    [Route("/")]
     public class Controller2 : ControllerBase
     {
-        [ApiController]
-        [Route("/")]
-        public class Controller1 : ControllerBase
+        [HttpGet("/model2")]
+        public ActionResult<Model2> Get()
         {
-            [HttpGet("/model2")]
-            public ActionResult<Model2> Get()
-            {
-                ICollection<string> models = new List<string>();
-                string model1 = "Ira";
-                string model2 = "Anna";
-                models.Add(model1);
-                models.Add(model2);
-                Model2 modelsEntity = new Model2(models, "Ivanova");
-                return Ok(modelsEntity);
+            ICollection<string> models = new List<string>();
+            string model1 = "Ira";
+            string model2 = "Anna";
+            models.Add(model1);
+            models.Add(model2);
+            Model2 modelsEntity = new Model2(models, "Ivanova");
+            return Ok(modelsEntity);
 
-            }
         }
     }
 }
